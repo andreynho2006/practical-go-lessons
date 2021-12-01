20934696526c2e5ab0651ccb0dd0c6a486cb9443
andreicirlan <andreynho2006@gmail.com>
2021-12-01 13:04:33 +0200

    add function to print room details

---
+++ functionsHotel.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+func main() {
+	const hotelName = "Gopher Paris Inn"
+	const totalRooms = 134
+	const firstRoomNumber = 110
+
+	rand.Seed(time.Now().UTC().UnixNano())
+	roomsOccupied := rand.Intn(totalRooms)
+	roomsAvailable := totalRooms - roomsOccupied
+
+	occupancyRate := (float32(roomsOccupied) / float32(totalRooms)) * 100
+	var occupancyLevel string
+	if occupancyRate > 70 {
+		occupancyLevel = "High"
+	} else if occupancyRate > 20 {
+		occupancyLevel = "Medium"
+	} else {
+		occupancyLevel = "Low"
+	}
+
+	fmt.Println("Hotel:", hotelName)
+	fmt.Println("Number of rooms", totalRooms)
+	fmt.Println("Rooms available", roomsAvailable)
+	fmt.Println("                  Occupancy Level:", occupancyLevel)
+	fmt.Printf("                  Occupancy Rate: %0.2f %%\n", occupancyRate)
+
+	if roomsAvailable > 0 {
+		fmt.Println("Rooms:")
+		for i := 0; roomsAvailable > i; i++ {
+			roomNumber := firstRoomNumber + i
+			size := rand.Intn(6) + 1
+			nights := rand.Intn(10) + 1
+			printRoomDetails(roomNumber, size, nights)
+		}
+	} else {
+		fmt.Println("No rooms available for tonight")
+	}
+
+}
+
+// displey information about a room
+func printRoomDetails(roomNumber, size, nights int) {
+	fmt.Println(roomNumber, ":", size, "people /", nights, " nights ")
+}
