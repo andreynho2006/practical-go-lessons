75f3d7dfe2c847c480f8dafec7406b1ec7f283ea
andreicirlan <andreynho2006@gmail.com>
2021-12-01 13:08:26 +0200

    add function to calculate the occupancy level

--- functionsHotel.go
+++ functionsHotel.go
@@ -16,14 +16,7 @@ func main() {
 	roomsAvailable := totalRooms - roomsOccupied
 
 	occupancyRate := (float32(roomsOccupied) / float32(totalRooms)) * 100
-	var occupancyLevel string
-	if occupancyRate > 70 {
-		occupancyLevel = "High"
-	} else if occupancyRate > 20 {
-		occupancyLevel = "Medium"
-	} else {
-		occupancyLevel = "Low"
-	}
+	occupancyLevel := occupancyLevel(occupancyRate)
 
 	fmt.Println("Hotel:", hotelName)
 	fmt.Println("Number of rooms", totalRooms)
@@ -48,4 +41,18 @@ func main() {
 // displey information about a room
 func printRoomDetails(roomNumber, size, nights int) {
 	fmt.Println(roomNumber, ":", size, "people /", nights, " nights ")
+}
+
+// retrieve occupancyLevel from an occupancyRate
+// from 0% to 30% occupancy rate return low
+// from 31% to 69% occupancy rate return medium
+// from 70% to 100% occupancy rate return high
+func occupancyLevel(occupancyRate float32) string {
+	if occupancyRate > 70 {
+		return "High"
+	} else if occupancyRate > 30 {
+		return "Medium"
+	} else {
+		return "Low"
+	}
 }
